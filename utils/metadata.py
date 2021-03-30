a39659f076d030051c23ef417467f40a945adc4e
Katie McLaughlin <katie@glasnt.com>
2021-03-30 14:14:16 +1100

    initial code

---
+++ utils/metadata.py
@@ -0,0 +1,47 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import request as flask_request
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+import requests
+
+METADATA_URI = "http://metadata.google.internal/computeMetadata/v1/"
+
+
+def get_project_id() -> str:
+    """Use the 'google-auth-library' to make a request to the metadata server or
+    default to Application Default Credentials in your local environment."""
+    _, project = google.auth.default()
+    return project
+
+
+def get_service_region() -> str:
+    """Get region from local metadata server
+    Region in format: projects/PROJECT_NUMBER/regions/REGION"""
+    slug = "instance/region"
+    data = requests.get(METADATA_URI + slug, headers={"Metadata-Flavor": "Google"})
+    return data.content
+
+
+def authenticated_request(url: str, method: str) -> str:
+    """Make a request with an ID token to a protected service
+    https://cloud.google.com/iap/docs/authentication-howto#iap_make_request-python"""
+    client_id = flask_request.host
+    open_id_connect_token = id_token.fetch_id_token(Request(), client_id)
+    resp = requests.request(
+        method, url, headers={"Authorization": f"Bearer {open_id_connect_token}"}
+    )
+    return resp.content
